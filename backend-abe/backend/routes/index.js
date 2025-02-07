0a5045c352b328dda6662fa1d394236f7b7b1cda
Rebiya <165892445+Rebiya@users.noreply.github.com>
2025-02-07 12:54:39 +0300

    Delete backend-abe/backend directory

--- backend-abe/backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-//import express module and call the router method to create a router
-const express = require("express");
-const router = express.Router();
-//import the install router
-const installRouter = require("./install.routes");
-//use the router to handle requests
-router.use(installRouter);
-//export the router
-module.exports = router;
