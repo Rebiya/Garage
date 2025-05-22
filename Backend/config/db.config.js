4e03d5efc1491dbefaaa2f67b2741d94b4c97899
Rebiya <rebum.19@gmail.com>
2025-05-22 18:02:41 +0300

    db config updated

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -2,21 +2,23 @@
 const mysql = require("mysql2/promise");
 // Prepare connection parameters we use to connect to the database
 const dbConfig = {
-  connectionLimit: 10,
+  // connectionLimit: 10,
   password: process.env.DB_PASS,
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
 };
 
-// const pool = mysql.createPool(dbConfig);
-// // Prepare a function that will execute the SQL queries asynchronously
-// async function query(sql, params) {
-//   const [rows, fields] = await pool.execute(sql, params);
-//   return rows;
+//check if successfully connected to the database based on the connection parameters
+const connection = mysql.createConnection(dbConfig);
+connection
+  .then(() => {
+    console.log("Connected to the database successfully");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database: ", err);
+  });
 
-// }
-// Create the connection pool
 const pool = mysql.createPool(dbConfig);
 // Create the query function that will execute the SQL queries asynchronously
 async function query(sql, params) {
